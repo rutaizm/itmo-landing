06528acca8ec97a73dedf0aa6ad498373cdec306
Stanislav Meleshchik <st.mele.a@yandex.ru>
2022-04-02 13:22:36 +0300

    feat: добавить массив карточек

---
+++ utils/teamCards.js
@@ -0,0 +1,44 @@
+export const publicationCards = [
+    {
+        id: 1,
+        image: '../images/team/Bobtsov.png',
+        name: 'Бобцов Алексей Алексеевич',
+        about: 'Директор, профессор, ДТН',
+    },
+    {
+        id: 2,
+        image: '../images/team/Bukhanosky.png',
+        name: 'Буханоский Александр Валерьевич',
+        about: 'Директор, руководитель, ДТН',
+    },
+    {
+        id: 3,
+        image: '../images/team/Muromtsev.png',
+        name: 'Муромцев Дмитрий Ильич',
+        about: 'Руководитель, доцент, КТН',
+    },
+    {
+        id: 4,
+        image: '../images/team/Matveev.png',
+        name: 'Матвеев Юрий Николаевич',
+        about: 'Руководитель, профессор, ДТН',
+    },
+    {
+        id: 5,
+        image: '../images/team/Shalyto.png',
+        name: 'Шалыто Анатолий Абрамович',
+        about: 'Профессор, ДТН',
+    },
+    {
+        id: 6,
+        image: '../images/team/Kolyubin.png',
+        name: 'Колюбин Сергей Алексеевич',
+        about: 'Начальник, профессор, ДТН',
+    },
+    {
+        id: 7,
+        image: '../images/team/Pyrkin.png',
+        name: 'Пыркин Антон Александрович',
+        about: 'Декан, профессор, ДТН',
+    },
+]
