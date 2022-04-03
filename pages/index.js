3d2033a7b0302620db2fe729eaf311b6aff97357
Tatyana Izmailova <rutaizm@yandex.ru>
2022-04-03 15:11:10 +0300

    added background photo, header and button

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,7 @@ import CardPublication from "../components/CardPublication.js";
 import PsevdoElement from "../components/PsevdoElement.js";
 import { publicationCards } from "../utils/publicationCards.js";
 import { cardContainer } from "../utils/constants.js";
-<<<<<<< HEAD
-=======
 import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
->>>>>>> 2c0d81f5a05f750c77a3ffbc9c2f131dbc6195ac
 
 const teamCardsContainer = document.querySelector('.team__cards')
 
@@ -25,17 +22,11 @@ const teamSlider = new Swiper('.team__slider', {
         el: '.team__slider-pagination',
         bulletClass: 'team__pagination-bullet',
         bulletActiveClass: 'team__pagination-bullet_active',
-<<<<<<< HEAD
-      clickable: true,
-      },
-      navigation: {
-=======
         clickable: true,
     },
 
     // Navigation arrows
     navigation: {
->>>>>>> 2c0d81f5a05f750c77a3ffbc9c2f131dbc6195ac
         nextEl: '.team__slider-button_next',
         prevEl: '.team__slider-button_prev',
     },
@@ -71,10 +62,6 @@ const publicationSlider = new Swiper('.publication__slider', {
     },
 });
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 2c0d81f5a05f750c77a3ffbc9c2f131dbc6195ac
 // Переделается в класс Section
 function renderTeamCards() {
     teamCards.forEach(card => {
@@ -97,25 +84,3 @@ renderPublicationCards()
 // ----------------------------------
 
 new PsevdoElement(publicationSlider, '.publication__card', 6).enabled()
-<<<<<<< HEAD
-
-// publicationSlider.on('sliderMove', (touchmove) => {
-//     console.log("событие sliderMove");
-// })
-// publicationSlider.on('transitionStart', () => {
-//     console.log("событие transitionStart");
-// })
-// publicationSlider.on('transitionEnd', () => {
-//     console.log("событие transitionEnd");
-// })
-// publicationSlider.on('touchMove', (touchMove) => {
-//     console.log("событие touchMove");
-// })
-// publicationSlider.on('transitionEnd', () => {
-//     console.log("событие transitionEnd");
-// })
-// publicationSlider.on('transitionEnd', () => {
-//     console.log("событие transitionEnd");
-// })
-=======
->>>>>>> 2c0d81f5a05f750c77a3ffbc9c2f131dbc6195ac
